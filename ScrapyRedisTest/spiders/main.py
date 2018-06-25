cacb899dee8578e213b6d6987d5dc1e6b6590c97
nanmuyao <1558249222han@gmail.com>
2018-06-25 16:34:38 +0800

    添加参数
    添加参数

--- ScrapyRedisTest/spiders/main.py
+++ ScrapyRedisTest/spiders/main.py
@@ -4,4 +4,5 @@ import sys
 import os
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+#execute(["scrapy", "crawl", "kkex_kline", '-atimeframe=1m']) 传递参数
 execute(["scrapy", "crawl", "jobbole"])
